a2c07f67b62547cd5a1653a61a787bc9fd528bd4
liushengtao <1316192871@qq.com>
2021-05-20 18:03:09 +0800

    0520
    0520

--- React/react_staging/react_staging/10._react_staging_9_NavLink的使用/src/App.jsx
+++ React/react_staging/react_staging/10._react_staging_9_NavLink的使用/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, Route } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Header from './component/Header'
@@ -25,12 +25,12 @@ export default class App extends Component {
                 Home
               </a> */}
               {/* 在react中靠路由链接实现切换组件---编写路由链接 */}
-              <Link className="list-group-item active" to="/home">
+              <NavLink activeClassName="atColor" className="list-group-item " to="/home">
                 Home
-              </Link>
-              <Link className="list-group-item " to="/about">
+              </NavLink>
+              <NavLink activeClassName="atColor" className="list-group-item " to="/about">
                 About
-              </Link>
+              </NavLink>
             </div>
           </div>
           <div className="col-xs-6">
