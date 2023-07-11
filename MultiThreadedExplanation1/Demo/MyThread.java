798a00e1f32f124ac8bacad8cef7cd6f7d549fc0
liushengtao <1316192871@qq.com>
2023-07-11 18:59:33 +0800

    feat: updaterq

--- MultiThreadedExplanation1/Demo/MyThread.java
+++ MultiThreadedExplanation1/Demo/MyThread.java
@@ -1,21 +1,21 @@
-package com;
+package MultiThreadedExplanation.Demo;
 
-public class ThreadDemo01 extends Thread {
-
+public class MyThread extends Thread {
     @Override
     public void run() {
-        for(int i=0;i<100;i++){
-            System.out.println("这是run方法"+i);
-        }
+       for(int i=0;i<100;i++){
+        System.out.println("这是run方法"+i);
+       }
     }
 
     public static void main(String [] args){
 
-        ThreadDemo01 myThread=new ThreadDemo01();
+        MyThread myThread=new MyThread();
         myThread.start();
 
         for(int i=1;i<1000;i++){
             System.out.println("这是main方法"+i);
         }
     }
+
 }
--- ThreadRltRunnable/src/com/ThreadDemo02.java
+++ ThreadRltRunnable/src/com/ThreadDemo02.java
@@ -1,2 +1,4 @@
-package com;public class ThreadDemo02 {
+package com;
+
+public class ThreadDemo02 {
 }
